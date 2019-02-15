88f9158c71ea02710192391caf0ee70852926b92
Stas <Alsagarrr@gmail.com>
2019-02-15 17:28:11 +0500

    for lesson 9

--- Lessons 9/yoga/js/script.js
+++ Lessons 9/yoga/js/script.js
@@ -128,10 +128,12 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 
-  //  Второе задание
+/*  //  Второе задание
     //<input id="age" value="30">
     let age = document.getElementById('age');
     function showUser(surname, name) {
         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
     }
-    showUser.apply(age, ["Mironov", "Stas"]);
+    showUser.apply(age, ["Mironov", "Stas"]);*/
+
+
